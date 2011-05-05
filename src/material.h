28566a0dcf5fddfcd7b3f17743ff233e2efaa027
Egor Lynko <flexoid@gmail.com>
2011-05-05 18:20:29 +0300

    Конструктор для Material без параметров

--- src/material.h
+++ src/material.h
@@ -6,6 +6,7 @@ class Material
 public:
     Material(float InLambert, float InPhong, float InTransp)
         : lambert(InLambert), phong(InPhong), transperancy(InTransp) {}
+    Material() : lambert(0), phong(0), transperancy(0) {}
     float lambert;
     float phong;
     float transperancy;
