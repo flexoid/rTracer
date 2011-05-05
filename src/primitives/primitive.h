3c9c5a7d5371a537e4b52f5aef95f7dd8b7fe43b
Egor Lynko <flexoid@gmail.com>
2011-05-05 18:21:07 +0300

    Класс Primitive теперь хранит материал

--- src/primitives/primitive.h
+++ src/primitives/primitive.h
@@ -2,11 +2,13 @@
 #define PRIMITIVE_H
 
 #include "../colorrgb.h"
+#include "../material.h"
 
 class Primitive
 {
 protected:
     ColorRGB color;
+    Material material;
 };
 
 #endif // PRIMITIVE_H
