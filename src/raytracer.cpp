bb034260f6463d55a7fb529089f6e1c317736d7f
Egor Lynko <flexoid@gmail.com>
2011-05-08 00:53:32 +0300

    Метод Trace в классе RayTracer

--- src/raytracer.cpp
+++ src/raytracer.cpp
@@ -1,11 +1,12 @@
-#include "raytracer.h"
 #include <math.h>
+#include "raytracer.h"
 
 RayTracer::RayTracer(Scene* scene) : scene(scene)
 {
 }
 
-Vector3 RayTracer::Refract(Primitive* primitive, Vector3 vector, Vector3 point){
+Vector3 RayTracer::Refract(Primitive* primitive, Vector3 vector, Vector3 point)
+{
     Vector3 norm = primitive->Norm(point);
     float nv = vector.DotProduct(norm);
     float a,bf;
@@ -24,10 +25,35 @@ Vector3 RayTracer::Refract(Primitive* primitive, Vector3 vector, Vector3 point){
         return Vector3(0,0,0);
     float b = bf*(nv*a + sqrt(D));
     return Vector3(vector*a + norm*b);
-
 }
-Vector3 RayTracer::Reflect(Primitive* primitive, Vector3 vector, Vector3 point){
+
+Vector3 RayTracer::Reflect(Primitive* primitive, Vector3 vector, Vector3 point)
+{
     Vector3 norm = primitive->Norm(point);
     float f = 2 * vector.DotProduct(norm);
     return Vector3(vector - norm*f);
+}
+
+Primitive* RayTracer::Trace(Ray ray, Vector3 &IntersectPoint)
+{
+    IntersectPoint = Vector3::Null();
+    Primitive* primitive;
+
+    std::vector< Primitive* >::iterator i;
+    i = scene->primitives.begin();
+    while (i != scene->primitives.end())
+    {
+        Primitive* prim = *i;
+        Vector3 point = prim->Trace(ray);
+        if (point != Vector3::Null() && (IntersectPoint == Vector3::Null() ||
+            point.DistanceFrom(ray.from) < IntersectPoint.DistanceFrom(ray.from)))
+        {
+            IntersectPoint = point;
+            primitive = prim;
+        }
+    }
+    if (IntersectPoint != Vector3::Null())
+        return primitive;
+    else
+        return 0;
 }
