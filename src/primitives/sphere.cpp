b73819147edcaf738c9d25fa276cac4f0a8dee58
Zhenya Ast <astzhe@gmail.com>
2011-05-07 22:49:52 +0300

    переработан класс Vector3

--- src/primitives/sphere.cpp
+++ src/primitives/sphere.cpp
@@ -20,10 +20,10 @@ Vector3 Sphere::Trace(Ray ray)
     Vector3 ac=center-ray.from;
     float acs=0;
     // Вычисляем коэффиценты в квадратном уравнении пересечения сферы и прямой
-    acs = ac.Dot(ray.dir);
+    acs = ac.ScalarProduct(ray.dir);
     if (acs < 0)
         return Vector3(0,0,0);
-    float sqrLength=ac.Dot(ac);
+    float sqrLength=ac.ScalarProduct(ac);
     // Вычисляем дискриминант, корни и находим точку пересечения
     float D=acs*acs-sqrLength+radius*radius;
     if (D < 0)
--- src/vector3.h
+++ src/vector3.h
@@ -40,8 +40,7 @@ public:
 
     inline Vector3 operator/ (float S ) const
     {
-        float fInv = 1.0f / S;
-        return Vector3 (x * fInv , y * fInv, z * fInv);
+        return Vector3 (x / S , y / S, z / S);
     }
 
     inline Vector3 operator/ (const Vector3& V2) const
@@ -81,44 +80,27 @@ public:
     }
 
     // Functions
-    inline float Dot( const Vector3 &V1 ) const
+    inline float ScalarProduct( const Vector3 &V1 ) const
     {
         return V1.x*x + V1.y*y + V1.z*z;
     }
 
-    inline Vector3 CrossProduct( const Vector3 &V2 ) const
-    {
-        return Vector3(
-                    y * V2.z  -  z * V2.y,
-                    z * V2.x  -  x * V2.z,
-                    x * V2.y  -  y * V2.x 	);
-    }
-
-    // Return vector rotated by the 3x3 portion of matrix m
-    Vector3 RotByMatrix( const float m[16] ) const
-    {
-        return Vector3(
-                    x*m[0] + y*m[4] + z*m[8],
-                    x*m[1] + y*m[5] + z*m[9],
-                    x*m[2] + y*m[6] + z*m[10] );
-    }
-
     // These require math.h for the sqrtf function
-    float Magnitude( ) const
+    float Length( ) const
     {
         return sqrtf( x*x + y*y + z*z );
     }
 
-    float Distance( const Vector3 &V1 ) const
+    float DistanceFrom( const Vector3 &V1 ) const
     {
-        return ( *this - V1 ).Magnitude();
+        return ( *this - V1 ).Length();
     }
 
     inline void Normalize()
     {
         float fMag = ( x*x + y*y + z*z );
-        if (fMag == 0) {return;}
-
+        if (fMag == 0)
+            return;
         float fMult = 1.0f/sqrtf(fMag);
         x *= fMult;
         y *= fMult;
