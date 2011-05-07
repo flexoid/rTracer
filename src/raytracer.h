6c3334e904bdf5ff831dd96e40de0e4fcd5e2a33
Egor Lynko <flexoid@gmail.com>
2011-05-07 23:11:36 +0300

    Добавлены прототипы функций в класс Raytracer

--- src/raytracer.h
+++ src/raytracer.h
@@ -2,11 +2,17 @@
 #define RAYTRACER_H
 
 #include "scene.h"
+#include "primitives/primitive.h"
+#include "ray.h"
+#include "light.h"
 
 class RayTracer
 {
 public:
     RayTracer(Scene*);
+    Primitive* Trace(Ray ray, Vector3& IntersectPoint);
+    bool InShadow(Vector3 point, Light light);
+
 private:
     Scene* scene;
 };
