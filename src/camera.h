cef12027919df885c61e750e0a9085736573f1ff
Zhenya Ast <astzhe@gmail.com>
2011-05-11 00:15:18 +0300

    Добавлен класс Camera

---
+++ src/camera.h
@@ -0,0 +1,32 @@
+#ifndef CAMERA_H
+#define CAMERA_H
+
+#include "vector3.h"
+
+class Camera
+{
+public:
+    Camera(Vector3 from, Vector3 to, float w, float h)
+    {
+        SetParametrs(from,to,w,h);
+    }
+    Camera() : eye(Vector3::Null()), lt(Vector3::Null()), right(Vector3::Null()), down(Vector3::Null()) {}
+
+    void SetParametrs(Vector3 from, Vector3 to, float w, float h)
+    {
+        Vector3 s = to - from;
+        Vector3 r = s.CrossProduct(Vector3(0,0,1));
+        Vector3 b = s.CrossProduct(r);
+        lt = from + s + (-w/2.0f)*r + (-h/2.0f)*b;
+        right = w*r;
+        down = h*b;
+        eye = from;
+    }
+
+    Vector3 eye;
+    Vector3 lt;
+    Vector3 right;
+    Vector3 down;
+};
+
+#endif // CAMERA_H
