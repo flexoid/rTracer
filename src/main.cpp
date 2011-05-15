e858aa5a6fb41db6138b10cbf40f62bcd8f4f2e6
Egor Lynko <flexoid@gmail.com>
2011-05-15 23:11:44 +0300

    Добавлены необходимые объекты для тестирования

--- src/main.cpp
+++ src/main.cpp
@@ -3,12 +3,34 @@
 #include <SDL/SDL.h>
 
 #include "engine.h"
+#include "scene.h"
+#include "primitives/sphere.h"
 
 int main()
 {
+    Material mat1(0.5, 0.5, 0.0, 1.0);
+    //Material mat2(0.1, 0.9, 0.3, 0.4);
+    //Material mat3(0.9, 0.1, 0.2, 0.2);
+
+    //Sphere* sph1 = new Sphere(Vector3(2.0, 2.0, 3.0), 3.0, ColorRGB(200, 70, 15), mat1);
+    Sphere* sph2 = new Sphere(Vector3::Null(), 1.0, ColorRGB(150, 40, 15), mat1);
+    //Sphere* sph3 = new Sphere(Vector3(-6.0, 2.0, 3.0), 4.0, ColorRGB(250, 90, 40), mat3);
+
+    Light* light = new Light(Vector3(-2.0, 10.0, 7.0), 3.0);
+    Camera* camera = new Camera(Vector3(5.0, 5.0, 5.0), Vector3::Null(), 1, 1);
+
+    Scene* scene = new Scene(ColorRGB(20, 60, 75));
+    //scene->AddPrimitive(sph1);
+    scene->AddPrimitive(sph2);
+    //scene->AddPrimitive(sph3);
+    scene->AddLight(light);
+    scene->AddCamera(camera);
+
+    Screen* screen = new Screen(camera, scene, 640, 480);
+
     // Создаем объект Window
     Engine* engine = new Engine(640, 480);
-    engine->Init();
+    engine->Init(screen);
 
     // Цикл отрисовки
     while(engine->Run())
