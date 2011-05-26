899f8873dc7c81854824e8b165d9dc461a21b515
Nick Herman <gerkola@gmail.com>
2011-05-27 02:25:39 +0300

    Добавлена реализация засветки.

--- src/colorrgb.cpp
+++ src/colorrgb.cpp
@@ -4,9 +4,18 @@ ColorRGB::ColorRGB(Uint8 r, Uint8 g, Uint8 b) : r(r), g(g), b(b){};
 
 ColorRGB::ColorRGB( ) : r(0), g(0), b(0){};
 
-ColorRGB ColorRGB::operator+(const ColorRGB& c2) const
+Uint16 min(Uint16 a, Uint16 b)
 {
-    return ColorRGB(r+c2.r, g+c2.g, b+c2.g);
+    return a<b?a:b;
+};
+
+ColorRGB ColorRGB::operator+(const ColorRGB& c2) const
+{   
+    Uint32 rr=0+r+c2.r, gg=0+g+c2.g, bb=0+b+c2.b,t;
+    if (rr>255) { gg=min(0+gg+rr-255,255); bb=min(0+bb+rr-255,255); rr=255;}
+    if (gg>255) { rr=min(0+rr+gg-255,255); bb=min(0+bb+gg-255,255); gg=255;}
+    if (bb>255) { rr=min(0+rr+bb-255,255); gg=min(0+gg+bb-255,255); bb=255;}
+    return ColorRGB(rr,gg,bb);
 };
 
 ColorRGB ColorRGB::operator*(const float k) const
--- src/main.cpp
+++ src/main.cpp
@@ -8,15 +8,15 @@
 
 int main()
 {
-    Material mat1(0.1, 0.3, 0.5, 0.3, 0.8);
-    Material mat2(0.2, 0.3, 0.2, 0.3, 0.8);
-    Material mat3(0.3, 0.3, 0.4, 0.2, 0.8);
+    Material mat1(0.1, 0.3, 0.5, 0.3, 1.0);
+    Material mat2(0.2, 0.3, 0.2, 0.3, 0.5);
+    Material mat3(0.3, 0.3, 0.4, 0.2, 0.1);
 
     Sphere* sph1 = new Sphere(Vector3(2.0f, 0.0f, 0.0f), 1.0f, ColorRGB(250, 0, 0), mat1);
     Sphere* sph2 = new Sphere(Vector3(0.0f, 0.0f, 0.0f), 1.0f, ColorRGB(0, 240, 0), mat2);
     Sphere* sph3 = new Sphere(Vector3(0.0f, 2.0f, 0.0f), 1.0f, ColorRGB(0, 0, 240), mat3);
 
-    Light* light = new Light(Vector3(0.5, 0.5, 11.0), 3.0);
+    Light* light = new Light(Vector3(0.5, 0.5, 11.0), 7.0);
     Camera* camera = new Camera(Vector3(5.0, 5.0, 4.0), Vector3::Null(), 1.0, 1.0);
 
     Scene* scene = new Scene(ColorRGB(20, 60, 75));
