60c97a64fc9d63b040ee01d077bedc144ac81c34
Egor Lynko <flexoid@gmail.com>
2011-05-27 19:28:55 +0300

    Убраны лишние приведения типов

--- src/colorrgb.cpp
+++ src/colorrgb.cpp
@@ -11,33 +11,49 @@ Uint16 min(Uint16 a, Uint16 b)
 
 ColorRGB ColorRGB::operator+(const ColorRGB& c2) const
 {
-    Uint32 rr=(Uint32) r+(Uint32) c2.r, gg=(Uint32) g+(Uint32) c2.g, bb=(Uint32) b+(Uint32) c2.b;
-    if (rr>255)
-        { gg=min(0+gg+rr-255,255); bb=min(0+bb+rr-255,255); rr=255;}
-    if (gg>255)
-        { rr=min(0+rr+gg-255,255); bb=min(0+bb+gg-255,255); gg=255;}
-    if (bb>255)
-        { rr=min(0+rr+bb-255,255); gg=min(0+gg+bb-255,255); bb=255;}
-    return ColorRGB(rr,gg,bb);
+    Uint32 rr = r + c2.r, gg = g + c2.g, bb = b + c2.b;
+    if (rr > 255) {
+        gg = min(gg + rr - 255, 255);
+        bb = min(bb + rr - 255, 255);
+        rr = 255;
+    }
+    if (gg > 255) {
+        rr = min(rr + gg - 255, 255);
+        bb = min(bb + gg - 255, 255);
+        gg = 255;
+    }
+    if (bb > 255) {
+        rr = min(rr + bb - 255, 255);
+        gg = min(gg + bb - 255, 255);
+        bb = 255;
+    }
+    return ColorRGB(rr, gg, bb);
  };
 
 ColorRGB ColorRGB::operator*(const float k) const
 {
-    Uint32 rr=(Uint32) r*k, gg=(Uint32) g*k, bb=(Uint32) b*k;
-    if (rr>255)
-        { gg=min(0+gg+rr-255,255); bb=min(0+bb+rr-255,255); rr=255;}
-    if (gg>255)
-        {rr=min(0+rr+gg-255,255); bb=min(0+bb+gg-255,255); gg=255;}
-    if (bb>255)
-        { rr=min(0+rr+bb-255,255); gg=min(0+gg+bb-255,255); bb=255;}
-    return ColorRGB(rr,gg,bb);
+    Uint32 rr = r * k, gg = g * k, bb = b * k;
+    if (rr > 255) {
+        gg = min(gg + rr - 255, 255);
+        bb = min(bb + rr - 255, 255);
+        rr = 255;
+    }
+    if (gg > 255) {
+        rr = min(rr + gg - 255, 255);
+        bb = min(bb + gg - 255, 255);
+        gg = 255;
+    }
+    if (bb > 255) {
+        rr = min(rr + bb - 255, 255);
+        gg = min(gg + bb - 255, 255);
+        bb = 255;
+    }
+    return ColorRGB(rr, gg, bb);
 };
 
 void ColorRGB::operator+=(const ColorRGB& c2)
 {
-    r+=c2.r;
-    g+=c2.g;
-    b+=c2.g;
+    *this = *this + c2;
 };
 
 ColorRGB ColorRGB::Null()
